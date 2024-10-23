97b244d16949900d9501993baf27f76224952d6b
Luan Leite <luanleite202@gmail.com>
2024-10-22 22:14:52 -0300

    Using Selections and BS4 to pratice

--- Http/code.py
+++ Http/code.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 response = requests.get("https://books.toscrape.com/")
@@ -8,8 +9,10 @@ content = response.content
 
 site = BeautifulSoup(content, "html.parser")
 
-post = site.find("li", attrs={"class":"col-xs-6 col-sm-4 col-md-3 col-lg-3"})
+section = site.find_all("li", attrs={"class":"col-xs-6 col-sm-4 col-md-3 col-lg-3"})
 
-dado = post.find("a", attrs={"title": "A Light in the Attic"})
-print(dado)
-
+for item in section:
+    titulos = item.find("h3")
+    valor = item.find("p", attrs={"class": "price_color"})
+    print(titulos.text, valor.text, sep="\n")
+    
